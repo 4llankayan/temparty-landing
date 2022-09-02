2e50f3cfa9b0b0fa115f6e310f99e7863e678349
Allan Kayan <4llankayan@gmail.com>
2022-09-01 23:29:40 -0300

    fix postgres and add initial css

--- src/db.js
+++ src/db.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv/config.js";
+import pg from 'pg';
 
 const sequelize = new Sequelize(
   process.env.DB_URL,
-  {dialect: 'postgres'}
+  {
+    dialect: 'postgres',
+    dialectModule: pg
+  }
 );
 
 export default sequelize;
