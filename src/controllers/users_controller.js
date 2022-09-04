56fda17b6eabfdb40b9b934bfddefd6aae440ac9
Allan Kayan <4llankayan@gmail.com>
2022-09-03 21:37:03 -0300

    fix user controller find

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -4,33 +4,26 @@ import jwt from "jsonwebtoken";
 
 async function register(req, res) {
   try {
-    // Get user input
     const { name, email, password } = req.body;
 
-    // Validate user input
     if (!(email && password && name)) {
       res.status(400).send("All input is required");
     }
 
-    // check if user already exist
-    // Validate if user exist in our database
-    // const oldUser = await User.findOne({ email });
+    const oldUser = await User.findOne({ where: { email: email }});
 
-    // if (oldUser != null) {
-    //   return res.status(409).send("User Already Exist. Please Login");
-    // }
+    if (oldUser != null) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
 
-    //Encrypt user password
     var encryptedPassword = await bcrypt.hash(password, 10);
 
-    // Create user in our database
     const user = await User.create({
       name,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      email: email.toLowerCase(),
       password: encryptedPassword,
     });
 
-    // Create token
     const token = jwt.sign(
       { user_id: user._id, email },
       process.env.TOKEN_KEY,
@@ -38,11 +31,10 @@ async function register(req, res) {
         expiresIn: "2h",
       }
     );
-    // save user token
+
     user.token = token;
     await user.save()
 
-    // return new user
     res.status(201).json(user);
   } catch (err) {
     console.log(err);
@@ -51,18 +43,16 @@ async function register(req, res) {
 
 async function login(req, res) {
   try {
-    // Get user input
     const { email, password } = req.body;
 
-    // Validate user input
     if (!(email && password)) {
       res.status(400).send("All input is required");
     }
-    // Validate if user exist in our database
-    const user = await User.findOne({ email });
+
+    const user = await User.findOne({ where: { email: email }});
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      // Create token
+
       const token = jwt.sign(
         { user_id: user._id, email },
         process.env.TOKEN_KEY,
@@ -71,14 +61,14 @@ async function login(req, res) {
         }
       );
 
-      // save user token
       user.token = token;
       user.save();
 
-      // user
       res.status(200).json(user);
+    } else {
+      res.status(400).send("Invalid Credentials");
     }
-    res.status(400).send("Invalid Credentials");
+
   } catch (err) {
     console.log(err);
   }
