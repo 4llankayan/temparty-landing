75bf8b5b66fdac5696412772864e6637b229d29a
Allan Kayan <4llankayan@gmail.com>
2022-12-14 02:51:41 -0300

    start new index

--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ routes.use(express.static('public'))
 routes
   .get('/', function(){
   })
-  .use(api);
+  .use(api)
+  .use(function(req, res) {
+    res.status(404).sendFile('404.html', { root: './public' });
+});
 
 export { routes as default };
